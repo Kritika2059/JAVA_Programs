47c678635dd80df092484cf843ae2dfee1c6a4f9
Kritika2059 <110281597+Kritika2059@users.noreply.github.com>
2024-04-25 16:19:38 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+            //write code here
+        System.out.println ("Hello World!!");
+        int num1 =5;
+        int num2 =6;
+        int num3 =7;
+        int sum = num1+ num2+ num3;
+        System.out.println(sum);
+    }
+}
