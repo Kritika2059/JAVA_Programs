be1189ef66289826987f83423da1a46b84b256bd
Kritika2059 <110281597+Kritika2059@users.noreply.github.com>
2024-05-09 17:15:00 +0545

    Add files via upload

---
+++ java_46_packages/java_46_package1.java
@@ -0,0 +1,10 @@
+package java_46_packages; //by this, we create a package named packages
+
+public class java_46_package1 {
+    public int add(int n1, int n2) {
+        return n1+n2;
+    }
+    public int sub(int n1, int n2) {
+        return n1-n2;
+    }
+}
---
+++ java_46_packages/java_46_package2.java
@@ -0,0 +1,7 @@
+package java_46_packages; //by this, we create a package named java_46_packages
+
+public class java_46_package2 {
+    public int mul(int n1, int n2) {
+        return n1 * n2;
+    }
+}
